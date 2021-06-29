1e4c06df452a775c5b1576ca64ccb38957d6d670
Freddy <youfreddi@gmail.com>
2021-06-29 03:07:20 +0200

    now uses praw nativly in psaw improved error handeling add key to abort

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import time
 import praw
 import sys
 import time
+import keyboard
 
 class DebugLevel(Enum):
     ALWAYS = 0
@@ -16,17 +17,18 @@ class DebugLevel(Enum):
     INFO = 3
     DEBUG = 4
 
-api = PushshiftAPI()
+prw = praw.Reddit(user_agent='praw_overflow',client_id='SLcx5BHOfpE3bQ',client_secret='fowGwZ-GXfjG2TKpzp3u0gH8HeINgQ')
+
+api = PushshiftAPI(prw)
 
 DEBUG_LEVEL = DebugLevel.INFO
 CHILL_TIME = 0.5
 
 
 subreddits = ['berlin']
-static_fieldnames = ['id','permalink','author', 'author_fullname', 'title', 'url', 'subreddit', 'stickied',  'created_utc', 'is_original_content','author_flair_text','is_video','locked','selftext','link_flair_richtext','retrieved_on','domain','over_18']
+static_fieldnames = ['id','permalink','author', 'author_fullname', 'title', 'url', 'subreddit', 'stickied',  'created_utc', 'is_original_content','author_flair_text','is_video','locked','selftext','link_flair_richtext','domain','over_18']
 dynamic_fieldnames = ['score','total_awards_received','upvote_ratio', 'num_comments']
 
-r = praw.Reddit(user_agent='praw_overflow',client_id='SLcx5BHOfpE3bQ',client_secret='fowGwZ-GXfjG2TKpzp3u0gH8HeINgQ')
 
 
 def escape(input):
@@ -48,7 +50,7 @@ for subreddit in subreddits:
 
     start_epoch = int(dt.datetime(2020, 1, 1).timestamp())
     end_epoch = int(dt.datetime(2020, 12, 31).timestamp())
-    interval = 2*60*60
+    interval = 84*60*60
 
     posts = list()
 
@@ -57,31 +59,9 @@ for subreddit in subreddits:
 
     num_of_epochs = (end_epoch - start_epoch) // interval
     num_of_epochs_received = 0
-    epoch_process_times = [1]
+    epoch_process_times = [10]
 
-    while current_epoch < end_epoch:
-        start_time = datetime.now()
-        result = list(api.search_submissions(after=current_epoch, before=current_epoch+interval, subreddit=subreddit, filter=static_fieldnames, limit=100, sort_type='created_utc', sort='asc'))
-        posts.extend(result)
-        num_of_epochs_received += 1
-        if len(result) >= 100:
-            warning_result_overflow += 1
-            debug("WARNING!")
-        current_epoch += interval
-        debug("Received epoch "+datetime.fromtimestamp(current_epoch).strftime("%c")+" to "+datetime.fromtimestamp(current_epoch+interval).strftime("%c"), DEBUG_LEVEL.DEBUG)
         
-        debug("\rReceived epoch "+str(num_of_epochs_received)+"/"+str(num_of_epochs)+"\t|\tEstimated time remaining: "+str(round((num_of_epochs-num_of_epochs_received)*(CHILL_TIME+sum(epoch_process_times) / len(epoch_process_times))))+" sek", DEBUG_LEVEL.ALWAYS, True)        
-        
-        time.sleep(CHILL_TIME)
-        epoch_process_times.append((datetime.now()-start_time).total_seconds())
-
-    print ('\nReceived static data for posts in r/'+subreddit+': '+ str(len(posts)))
-
-    if(warning_result_overflow > 0):
-        debug("Possible epoch overflow: "+warning_result_overflow)
-
-    header = static_fieldnames+dynamic_fieldnames+['datetime']
-    
     suffix = 0
     path = ""
     while True:
@@ -90,7 +70,13 @@ for subreddit in subreddits:
             debug("Writing r/"+subreddit+" to: "+path)
             break
         suffix += 1
-            
+        
+    header = static_fieldnames+dynamic_fieldnames+['epoch','datetime']
+    
+    post_processed = 0
+    post_process_times = [1]
+
+    errors = list()
 
     with open(path, mode='w', newline='', encoding='utf-8') as post_file:
         post_writer = csv.writer(post_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
@@ -98,37 +84,74 @@ for subreddit in subreddits:
         prev_month = datetime.fromtimestamp(start_epoch).month
         prev_day = datetime.fromtimestamp(start_epoch).day
 
-        post_processed = 0
-        post_process_times = [1]
 
-        for post in posts:
+        while current_epoch < end_epoch:
             start_time = datetime.now()
-            post_as_csv = []
-            for fieldname in static_fieldnames:
-                if hasattr(post, fieldname):
-                    post_as_csv.append(escape(getattr(post, fieldname)))
-                elif fieldname == 'author_fullname':
-                    post_as_csv.append('NULL')
-                else:
-                    debug("Warning: "+fieldname+' not found in: '+post.id)
-                    debug(post, DebugLevel.DEBUG)
-                    post_as_csv.append('NULL')
-            dynamic_post = r.submission(id=getattr(post, 'id'))
-            for fieldname in dynamic_fieldnames:
-                if hasattr(dynamic_post, fieldname):
-                    post_as_csv.append(escape(getattr(dynamic_post, fieldname)))
-                else:
-                    debug("Warning: "+fieldname+' not found in:'+post.id)
-                    debug(dynamic_post, DebugLevel.DEBUG)
-                    post_as_csv.append('NULL')
-            post_datetime = datetime.fromtimestamp(post.created_utc)
-            post_as_csv.append(post_datetime)
-            post_writer.writerow(post_as_csv)
-            post_process_times.append((datetime.now()-start_time).total_seconds())
+            result = list(api.search_submissions(after=current_epoch, before=current_epoch+interval, subreddit=subreddit, filter=static_fieldnames, limit=200, sort_type='created_utc', sort='asc'))
+            posts.extend(result)
+            num_of_epochs_received += 1
+            if len(result) >= 200:
+                warning_result_overflow += 1
+                errors.append(type('obj', (object,), {'type': 'EpochOverflow', 'epoch' : num_of_epochs_received}))
+            current_epoch += interval
+            debug("Received epoch "+datetime.fromtimestamp(current_epoch).strftime("%c")+" to "+datetime.fromtimestamp(current_epoch+interval).strftime("%c"), DEBUG_LEVEL.DEBUG)
+            
+             
+            #time.sleep(CHILL_TIME)
+            current_posts_processed = 0
+            post_processed = 0
 
-            post_processed += 1
-            debug("Processed posts "+str(post_processed)+"/"+str(len(posts))+"\t|\t"+"Estimated time remaining: "+str(round((len(posts)-post_processed)*(sum(post_process_times) / len(post_process_times))))+" sek", DEBUG_LEVEL.ALWAYS, True)        
+            this_post_process_times = [1]
+            for post in result:
+                try:
+                    if keyboard.is_pressed('c'):
+                        break
+                    start_time = datetime.now()
+                    post_as_csv = []
+                    post.datetime = datetime.fromtimestamp(post.created_utc)
+                    post.epoch = num_of_epochs_received
+                    for fieldname in header:
+                        if hasattr(post, fieldname):
+                            post_as_csv.append(escape(getattr(post, fieldname)))
+                        elif fieldname == 'author_fullname':
+                            post_as_csv.append('NULL')
+                        else:
+                            debug("Warning: "+fieldname+' not found in: '+post.id, DebugLevel.DEBUG)
+                            debug(post, DebugLevel.DEBUG)
+                            errors.append(type('obj', (object,), {'type': 'PostAttrNotFound', 'post' : post}))
+                            post_as_csv.append('NULL')
+                    post_writer.writerow(post_as_csv)
+                    this_post_process_times.append((datetime.now()-start_time).total_seconds())
 
-    debug('\nReceived dynamic data for posts in r/'+subreddit)
+                    post_processed += 1
+                    eta_posts = ((len(result)-post_processed)*(sum(this_post_process_times) / len(this_post_process_times)))
+                    eta = round((num_of_epochs-num_of_epochs_received)*(CHILL_TIME+sum(epoch_process_times) / len(epoch_process_times))+ eta_posts )
+                    debug("\rReceived epoch "+str(num_of_epochs_received)+"/"+str(num_of_epochs)+"   \t|\tProcessed posts "+str(post_processed)+"/"+str(len(result))+"    \t|\t   Estimated time remaining: "+str(eta)+" sek   ", DEBUG_LEVEL.ALWAYS, True)        
+                except OSError as err:
+                    print("OS error: {0}".format(err))
+                    errors.append(type('obj', (object,), {'type': 'UnknownError', 'epoch' : num_of_epochs_received}))
+            post_process_times.append(this_post_process_times)
+            #debug("Processed posts "+str(post_processed)+"/"+str(len(posts))+"\t|\t"+"Estimated time remaining: "+str(round((len(posts)-post_processed)*(sum(post_process_times) / len(post_process_times))))+" sek", DEBUG_LEVEL.ALWAYS, True)        
+
+            epoch_process_times.append((datetime.now()-start_time).total_seconds())
+
+
+            if keyboard.is_pressed('c'):
+                print('Aborting...')
+                debug('Latest post retrieved: '+str(datetime.fromtimestamp(max(post.created_utc for post in posts))), DebugLevel.ALWAYS)
+                break
+
+    debug('\nReceived for posts in r/'+subreddit+': '+ str(len(posts)))
+
+    if(warning_result_overflow > 0):
+        for error in errors:
+            if(error.type == 'EpochOverflow'):
+                debug('EpochOverflow Error in epoch: '+str(error.epoch), DebugLevel.ALWAYS)
+            elif(error.type == 'PostAttrNotFound'):
+                debug('PostAttrNotFound Error in post: '+str(error.post), DebugLevel.ALWAYS)
+            elif(error.type == 'UnknownError'):
+                debug('UnknownError Error in epoch: '+str(error.epoch), DebugLevel.ALWAYS)
+            
+
     debug('Finished r/'+subreddit+'\n')
 debug('Finished')
