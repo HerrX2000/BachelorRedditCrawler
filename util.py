3ab668ec4ac9acd1abedb450f19edce6c5e79175
Freddy <youfreddi@gmail.com>
2021-08-22 11:27:48 +0200

    util.download_file primitive verification of file with bytes size and attempt to support resuming downloads

--- util.py
+++ util.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import requests
 import os
 from clint.textui import progress
+import threading
 
 class DebugLevel(Enum):
     ALWAYS = 0
@@ -34,7 +35,7 @@ def debug(input, debug_level_input = DebugLevel.ALWAYS, self_updating = False, w
             print(input)
     if write_log:
         with open(log_file, "a") as myfile:
-            txt = str(datetime.now()) + " | [" + debug_level_input.name + "]:  \t" + str(input) + "\n"
+            txt = str(datetime.now()) + " | thread: " + threading.currentThread().name[:3] + " \t| [" + debug_level_input.name + "]:  \t" + str(input) + "\n"
             myfile.write(txt)
 
 def fix_len_int(int,len):
@@ -58,17 +59,33 @@ class DownloadedFile:
 
 def download_file(url, force = False, chunk_size = 16384, debug_level = DebugLevel.LOG_ONLY, progress_bar = True) -> DownloadedFile:
     local_filename = './downloads/'+url.split('/')[-1]
-    from_cache = False
     # NOTE the stream=True parameter below
-    if(os.path.isfile(local_filename) and os.stat(local_filename).st_size != 0 and not force):
-        debug(url+' already downloaded and using local file: '+local_filename, debug_level)
-        return DownloadedFile(local_filename, True)
+    
 
-    debug('Starting download of: '+url, debug_level)
-    with requests.get(url, stream=True) as r:
-        total_length = int(r.headers.get('content-length'))
+
+    request = requests.get(url, stream=True)
+    resume_byte_position = None
+    total_length = None
+    resume = False
+
+    total_length = int(request.headers.get('content-length'))
+    if os.path.isfile(local_filename)  and not force:
+        if os.stat(local_filename).st_size == total_length:
+            debug(url+' already downloaded and using local file: '+local_filename, debug_level)
+            return DownloadedFile(local_filename, True)
+        elif os.stat(local_filename).st_size != 0:
+            resume_header = {'Range': 'bytes=%d-' % os.stat(local_filename).st_size}
+            request = requests.get(url, headers=resume_header, stream=True, allow_redirects=True)
+            if request.status_code == 206:
+                resume = True
+                debug(url+' partialy downloaded and continuing now.')
+            else:
+                raise IOError('Requested partial download but server does not support it.') 
+
+    with request as r:
+        debug('Starting download of: '+url, debug_level)
         r.raise_for_status()
-        with open(local_filename, 'wb') as f:
+        with open(local_filename, 'ab' if resume else 'wb') as f:
             if(progress_bar):
                 for chunk in progress.bar(r.iter_content(chunk_size=chunk_size), expected_size=(total_length/chunk_size) + 1): 
                     # If you have chunk encoded response uncomment if
